7de3b75f5caf16395eab35459b4b048e0712e60a
Jan Collijs <jan.collijs@inuits.eu>
2013-10-04 11:11:34 +0200

    Added check on existing service and possibility to kill it

--- fnotify.sh
+++ fnotify.sh
@@ -4,9 +4,9 @@
 # http://thorstenl.blogspot.be/2007/01/thls-irssi-notification-script.html
 # Author: Jan Collijs
 # 
-# # You could use a different notifier depending on which window manager you use like:
-# # mate-notify-send -i gtk-dialog-warning -- "${heading}" "${message}"
-# # notify-send -i gtk-dialog-warning -- "${heading}" "${message}"
+# You could use a different notifier depending on which window manager you use like:
+# mate-notify-send -i gtk-dialog-warning -- "${heading}" "${message}"
+# notify-send -i gtk-dialog-warning -- "${heading}" "${message}"
 
 server='YOUR SERVER ADDRESS'
 
@@ -19,11 +19,12 @@ This script reads highlighted messages from irssi server.
 
 OPTIONS:
    -h      Show this message
-   -s      Start monitoring irssi server
+   -s      Start monitoring irssi messages
+   -k      Stop monitoring irssi messages
 EOF
 }
 
-while getopts "hs" flag
+while getopts "hsk" flag
 do
   case "$flag" in
     h)
@@ -31,7 +32,16 @@ do
       exit
     ;;
     s) 
-         ssh ${server} "tail -n 10 .irssi/fnotify ; : > .irssi/fnotify ; tail -f .irssi/fnotify " | sed -u 's/[<@&]//g' | while read heading message; do ratpoison -c "echo ${heading}: ${message}"; done &   
+        serviceState=$(ps -ef | grep .irssi/fnotify | grep -v grep | awk '{print $2}')
+        if [ -z "$serviceState" ]; then
+	  ssh ${server} "tail -n 10 .irssi/fnotify ; : > .irssi/fnotify ; tail -f .irssi/fnotify " | sed -u 's/[<@&]//g' | while read heading message; do ratpoison -c "echo ${heading}: ${message}"; done &   
+	fi
+    ;;
+    k)
+      serviceState=$(ps -ef | grep .irssi/fnotify | grep -v grep | awk '{print $2}')
+        if [ ! -z "$serviceState" ]; then
+          kill $serviceState
+      fi
     ;;
     *)
       usage
