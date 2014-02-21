223a643c4d188adb4be985617e78931b1be1466d
Jan Collijs <jan.collijs@inuits.eu>
2014-02-21 17:00:58 +0100

    Bugfix: cleaned up script

--- clean-repository.sh
+++ clean-repository.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 #
-# This script will cycle through a directory and only leaves the # most recent versions of a package in the directory
-# the other ones will be removed
+# Author: Jan Collijs
+# URL: https://github.com/visibilityspots/scripts#clean-repositorysh
 #
-# Parameters:
-#	repository-directory
-#	# version to keep
+# This script will cycle through a repository directory containing architecture subdirectories and looking for rpm packages older then X
 #
-# Example:
-#	./clean-repository.sh /path/to/rpm/directory 2
+# | EL6Server
+# | - i686
+# | - noarch
+# | - x86_64
 #
-#	This will remove every version of a package older then 2
+# Those architecture subdirectories exits of a series of subdirectories, for every piece of software a subdirectory has been created
+# Subdirectories in those architure dirs containing *repo* in their name will be skipped by this script.
+# The rpm packages created by automated jobs needs to follow the semantic versioning scheme (http://semver.org/) and
+# may NOT contain numbers as name
+#
+# For example:
+# | EL6Server
+# | - x86_68
+# | -- monitis-agent
+# | --- monitis-agent-4.2.38.x86_64.rpm
+# | --- monitis-agent-4.2.37.x86_64.rpm
+# | --- monitis-agent-4.2.36-43.x86_64.rpm
+#
+# Parameters used by this script are the path to the directory containing the architecture dirs and the number of packages to keep:
+#
+# $ ./clean-repository.sh /dir/to/yum/repository/EL6Server 3
+#
+# By default the script will only ouput a list of files, to actually remove those old packages uncomment line 84 :)
 
 # Initialize variables
 REPO=$(echo $1 | sed -e 's+/$++g')
@@ -19,34 +36,50 @@ VERSIONSTOKEEP=$(echo $2 | sed -e 's+/$++g')
 # Go to the repo directory to clean up
 cd $REPO
 
-# Grep all rpm files and chop off the version numbers
+# Select the architecture directories to clean
 shopt -s nullglob
-for RPM in *.rpm
+for DIR in $(ls -d ${REPO}/*/);
 do
-        RPM=`echo $RPM | sed -e 's/[[:digit:]].*$//g' | sed -e 's/-$//g'`
-        FILES=("${FILES[@]}" $RPM)
-done
-
-
-## Initialize an array with only the unique file names
-uniqueFiles=$(echo "${FILES[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')
-
-## Catch all packages older then 3 versions
-for file in $uniqueFiles
-do
-        # Calculate the old packages to remove 
-	TOTAL=$(find $file* -type f -exec stat --format '%Y :%y %n' {} \; | sort -nr | cut -d: -f2- | cut -d ' ' -f4 | wc -l)
-        OLDPACKAGES="$(( $TOTAL - $VERSIONSTOKEEP ))"
-
-        # Remove the calculated old packages if there are calculate
-        if [ "$OLDPACKAGES" -gt 0 ];then
-                ARCHIVEPACKAGES=$(find $file* -type f -exec stat --format '%Y :%y %n' {} \; | sort -nr | cut -d: -f2- | cut -d ' ' -f4 | tail -$OLDPACKAGES)
-                for archive in $ARCHIVEPACKAGES
+  if [[ "$DIR" != *repo* ]]; then
+          for SUBDIR in $(ls -d ${DIR}*/);
+          do
+                # Select the subdirectories which contains rpm packages to clean
+                if [[ "$SUBDIR" != *repo* ]] ; then
+                unset FILES
+                # Grep all rpm files and chop off the version numbers
+                for RPM in $SUBDIR*.rpm
                 do
-			# The action for each old package 
-                	rm $archive -rf
-                	echo $archive "package has been removed"
+                        RPM=`echo $RPM | awk -F / '{print $NF}'`
+                        RPM=`echo $RPM | sed -e 's/[[:digit:]].*$//g' | sed -e 's/-$//g'`
+                        FILES=("${FILES[@]}" $RPM)
                 done
-        fi
-done
 
+                ## Initialize an array with only the unique file names
+                uniqueFiles=$(echo "${FILES[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')
+
+                ## Catch all packages older then 3 versions
+                for file in $uniqueFiles
+                do
+			# Calculate the old packages to remove
+                        TOTAL=$(find ${SUBDIR}$file* -type f -exec stat --format '%Y :%y %n' {} \; | sort -nr | cut -d: -f2- | cut -d ' ' -f4 | wc -l)
+                        OLDPACKAGES="$(( $TOTAL - $VERSIONSTOKEEP ))"
+
+                        # Remove the calculated old packages if there are
+                        if [ "$OLDPACKAGES" -gt 0 ];then
+                                ARCHIVEPACKAGES=$(find ${SUBDIR}$file* -type f -exec stat --format '%Y :%y %n' {} \; | sort -nr | cut -d: -f2- | cut -d ' ' -f4 | tail -$OLDPACKAGES)
+                                ## Colored title
+                                echo -e "\n\e[1;34m[\e[00m --- \e[00;32mClean repository directory: ${SUBDIR} --- \e[1;34m]\e[00m\n"
+                                echo "$file: total: $TOTAL | old: $OLDPACKAGES"
+                                for archive in $ARCHIVEPACKAGES
+                                do
+                                        # The action for each old package
+#                                       sudo rm $archive -rf
+                                        echo "* $archive package has been removed"
+                                done
+                                echo "---------------------------------------------------------------------------------------------"
+                        fi
+                done
+                fi
+          done
+  fi
+done
